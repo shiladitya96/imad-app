ffcbe0e38ca48b8a3dad2e5227bd75afbedcf10a
shiladitya96 <30996411+shiladitya96@users.noreply.github.com>
2017-08-15 11:52:57 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+
 app.get('/a-one', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'a-one.html'));
 });
